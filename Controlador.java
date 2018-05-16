53e264ddb0b27e9123c51c0ff5e1de7920db5d6a
CatalinaSilva <38567419+CatalinaSilva@users.noreply.github.com>
2018-05-16 14:05:46 -0400

    commit
    ahora pregunta si puede despegar y si cumple las funciones

--- Controlador.java
+++ Controlador.java
@@ -15,13 +15,22 @@ import java.util.Random;
 
             robot.iniciarPista();
 
+            int m=1;
 
+            while(m==1) {
 
+                robot.getEstado();
+                m = robot.setDespegarModoAvion(robot.panel.getVelocidad(), robot.panel.getAltura(), robot.panel.getL_pista());
+                robot.condiciones_Despegar();
+            }
             robot.getEstado();
+            robot.Despegar();
+
+
             robot.setCambiar(1);
             int cont = 0;
             do {
-               estado= robot.movimientosSuelo(cont);
+               estado= robot.movimientosAire();
 
             }while (estado=true);
 
